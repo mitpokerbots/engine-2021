fbcfa34e0fb761496e7c2ac2ad0f3eac51476fed
Shreyas Srinivasan <shreyass@mit.edu>
2020-12-30 01:41:34 -0500

    Make starting stack 200

--- java_skeleton/javabot/skeleton/State.java
+++ java_skeleton/javabot/skeleton/State.java
@@ -5,7 +5,7 @@ package javabot.skeleton;
  */
 public class State {
     public static final int NUM_ROUNDS     = 1000;
-    public static final int STARTING_STACK = 400;
+    public static final int STARTING_STACK = 200;
     public static final int BIG_BLIND      = 2;
     public static final int SMALL_BLIND    = 1;
 }
