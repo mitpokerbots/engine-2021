b467c38bce16cdd0dc7b9185c0f8ab21edbc9d09
Shreyas Srinivasan <shreyass@mit.edu>
2021-01-01 23:19:17 -0500

    Implement RoundState in Java

--- java_skeleton/javabot/skeleton/RoundState.java
+++ java_skeleton/javabot/skeleton/RoundState.java
@@ -15,17 +15,15 @@ import java.lang.String;
 public class RoundState extends State {
     public final int button;
     public final int street;
-    public final List<Integer> pips;
     public final List<Integer> stacks;
     public final List<List<String>> hands;
-    public final List<String> deck;
+    public List<State> boardStates;
     public final State previousState;
 
-    public RoundState(int button, int street, List<Integer> pips, List<Integer> stacks,
-                      List<List<String>> hands, List<String> deck, State previousState) {
+    public RoundState(int button, int street, List<Integer> stacks, List<List<String>> hands,
+                      List<State> boardStates, State previousState) {
         this.button = button;
         this.street = street;
-        this.pips = Collections.unmodifiableList(pips);
         this.stacks = Collections.unmodifiableList(stacks);
         this.hands = Collections.unmodifiableList(
             Arrays.asList(
@@ -33,7 +31,7 @@ public class RoundState extends State {
                 Collections.unmodifiableList(hands.get(1))
             )
         );
-        this.deck = Collections.unmodifiableList(deck);
+        this.boardStates = boardStates;
         this.previousState = previousState;
     }
 
@@ -41,49 +39,51 @@ public class RoundState extends State {
      * Compares the players' hands and computes payoffs.
      */
     public State showdown() {
-        return new TerminalState(Arrays.asList(0, 0), this);
+        ArrayList<TerminalState> terminalBoardStates = new ArrayList<TerminalState>();
+        for (State boardState : this.boardStates) {
+            if (boardState instanceof BoardState) {
+                terminalBoardStates.add((TerminalState)(((BoardState)boardState).showdown()));
+            } else {
+                terminalBoardStates.add((TerminalState)boardState);
+            }
+        }
+        return new TerminalState(Arrays.asList(0, 0), new RoundState(this.button, this.street, this.stacks, this.hands, terminalBoardStates, this));
     }
 
     /**
      * Returns the active player's legal moves.
      */
-    public Set<ActionType> legalActions() {
-        int active = this.button % 2;
-        int continueCost = this.pips.get(1-active) - this.pips.get(active);
-        if (continueCost == 0) {
-            // we can only raise the stakes if both players can afford it
-            boolean betsForbidden = ((this.stacks.get(0) == 0) | (this.stacks.get(1) == 0));
-            if (betsForbidden) {
-                return new HashSet<ActionType>(Arrays.asList(ActionType.CHECK_ACTION_TYPE));
+    public List<Set<ActionType>> legalActions() {
+        List<Set<ActionType>> output = new ArrayList<Set<ActionType>>();
+        for (State boardState : this.boardStates) {
+            if (boardState instanceof BoardState) {
+                output.add(boardState.legalActions(this.button, this.stacks));
+            } else {
+                output.add(new HashSet<ActionType>(Arrays.asList(ActionType.CHECK_ACTION_TYPE)));
             }
-            return new HashSet<ActionType>(Arrays.asList(ActionType.CHECK_ACTION_TYPE, ActionType.RAISE_ACTION_TYPE));
         }
-        // continueCost > 0
-        // similarly, re-raising is only allowed if both players can afford it
-        boolean raisesForbidden = ((continueCost == this.stacks.get(active)) | (this.stacks.get(1-active) == 0));
-        if (raisesForbidden) {
-            return new HashSet<ActionType>(Arrays.asList(ActionType.FOLD_ACTION_TYPE, ActionType.CALL_ACTION_TYPE));
-        }
-        return new HashSet<ActionType>(Arrays.asList(ActionType.FOLD_ACTION_TYPE,
-                                                     ActionType.CALL_ACTION_TYPE,
-                                                     ActionType.RAISE_ACTION_TYPE));
+        return output;
     }
 
     /**
-     * Returns a list of the minimum and maximum legal raises.
+     * Returns a list of the minimum and maximum legal raises summed across boards.
      */
     public List<Integer> raiseBounds() {
         int active = this.button % 2;
-        int continueCost = this.pips.get(1-active) - this.pips.get(active);
-        int maxContribution = Math.min(this.stacks.get(active), this.stacks.get(1-active) + continueCost);
-        int minContribution = Math.min(maxContribution, continueCost + Math.max(continueCost, State.BIG_BLIND));
-        return Arrays.asList(this.pips.get(active) + minContribution, this.pips.get(active) + maxContribution);
+        return Arrays.asList(0, this.stacks.get(active));
     }
 
     /**
      * Resets the players' pips and advances the game tree to the next round of betting.
      */
     public State proceedStreet() {
+        for (int i = 0; i < State.NUM_BOARDS; i++) {
+            if (this.boardStates.get(i) instanceof BoardState) {
+                BoardState bs = (BoardState)this.boardStates.get(i);
+                bs.updatePot();
+                this.boardStates.set(i, bs);
+            }
+        }
         if (this.street == 5) {
             return this.showdown();
         }
@@ -93,56 +93,48 @@ public class RoundState extends State {
         } else {
             newStreet = this.street + 1;
         }
-        return new RoundState(1, newStreet, Arrays.asList(0, 0), this.stacks, this.hands, this.deck, this);
+        List<State> newBoardStates = new ArrayList<State>();
+        for (State oldBoardState : this.boardStates) {
+            if (oldBoardState instanceof BoardState) {
+                BoardState bs = (BoardState)oldBoardState;
+                newBoardStates.add(new BoardState(bs.pot, Arrays.asList(0, 0), bs.hands, bs.deck, oldBoardState));
+            } else {
+                newBoardStates.add(oldBoardState);
+            }
+        }
+        return new RoundState(1, newStreet, this.stacks, this.hands, newBoardStates, this);
     }
 
     /**
-     * Advances the game tree by one action performed by the active player.
+     * Advances the game tree by one list of action performed by the active player.
      */
-    public State proceed(Action action) {
+    public State proceed(List<Action> actions) {
+        List<State> newBoardStates = new ArrayList<State>();
+        for (int i = 0; i < State.NUM_BOARDS; i++) {
+            if (this.boardStates.get(i) instanceof BoardState) {
+                newBoardStates.add(((BoardState)this.boardStates.get(i)).proceed(actions.get(i), this.button, this.street));
+            } else {
+                newBoardStates.add(this.boardStates.get(i));
+            }
+        }
         int active = this.button % 2;
-        switch (action.actionType) {
-            case FOLD_ACTION_TYPE: {
-                int delta;
-                if (active == 0) {
-                    delta = this.stacks.get(0) - State.STARTING_STACK;
-                } else {
-                    delta = State.STARTING_STACK - this.stacks.get(1);
-                }
-                return new TerminalState(Arrays.asList(delta, -1 * delta), this);
+        List<Integer> newStacks = new ArrayList<Integer>(this.stacks);
+        int contribution = 0;
+        for (int i = 0; i < State.NUM_BOARDS; i++) {
+            if ((newBoardStates.get(i) instanceof BoardState) && (this.boardStates.get(i) instanceof BoardState)) {
+                contribution += ((BoardState)newBoardStates.get(i)).pips.get(active) - ((BoardState)this.boardStates.get(i)).pips.get(active);
             }
-            case CALL_ACTION_TYPE: {
-                if (this.button == 0) {  // sb calls bb
-                    return new RoundState(1, 0, Arrays.asList(State.BIG_BLIND, State.BIG_BLIND),
-                                          Arrays.asList(State.STARTING_STACK - State.BIG_BLIND,
-                                                        State.STARTING_STACK - State.BIG_BLIND),
-                                          this.hands, this.deck, this);
-                }
-                // both players acted
-                List<Integer> newPips = new ArrayList<Integer>(this.pips);
-                List<Integer> newStacks = new ArrayList<Integer>(this.stacks);
-                int contribution = newPips.get(1-active) - newPips.get(active);
-                newStacks.set(active, newStacks.get(active) - contribution);
-                newPips.set(active, newPips.get(active) + contribution);
-                RoundState state = new RoundState(this.button + 1, this.street, newPips, newStacks,
-                                                  this.hands, this.deck, this);
-                return state.proceedStreet();
-            }
-            case CHECK_ACTION_TYPE: {
-                if (((this.street == 0) & (this.button > 0)) | (this.button > 1)) {  // both players acted
-                    return this.proceedStreet();
-                }
-                // let opponent act
-                return new RoundState(this.button + 1, this.street, this.pips, this.stacks, this.hands, this.deck, this);
-            }
-            default: {  // RAISE_ACTION_TYPE
-                List<Integer> newPips = new ArrayList<Integer>(this.pips);
-                List<Integer> newStacks = new ArrayList<Integer>(this.stacks);
-                int contribution = action.amount - newPips.get(active);
-                newStacks.set(active, newStacks.get(active) - contribution);
-                newPips.set(active, newPips.get(active) + contribution);
-                return new RoundState(this.button + 1, this.street, newPips, newStacks, this.hands, this.deck, this);
-            }
+        }
+        newStacks.set(active, newStacks.get(active) - contribution);
+        ArrayList<Boolean> settled = new ArrayList<Boolean>();
+        for (State boardState : newBoardStates) {
+            settled.add((boardState instanceof TerminalState) || (((BoardState)boardState).settled));
+        }
+        State state = RoundState(this.button + 1, this.street, newStacks, this.hands, newBoardStates, this);
+        if (!settled.contains(false)) {
+            return state.proceedStreet();
+        } else {
+            return state;
         }
     }
 }
