dd5a77eae7e3c249f3f141f2011fad0f9604b358
Shreyas Srinivasan <shreyass@mit.edu>
2021-01-03 00:26:44 -0500

    Set num rounds to 500

--- python_skeleton/skeleton/states.py
+++ python_skeleton/skeleton/states.py
@@ -7,7 +7,7 @@ from .actions import FoldAction, CallAction, CheckAction, RaiseAction, AssignAct
 GameState = namedtuple('GameState', ['bankroll', 'opp_bankroll', 'game_clock', 'round_num'])
 TerminalState = namedtuple('TerminalState', ['deltas', 'previous_state'])
 
-NUM_ROUNDS = 1000
+NUM_ROUNDS = 500
 STARTING_STACK = 200
 BIG_BLIND = 2
 SMALL_BLIND = 1
